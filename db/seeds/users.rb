6469282430f3f377a54e08990e1992a99d4e1283
hal13 <a15z8my@aiit.ac.jp>
2017-06-06 14:56:27 +0000

    modify count user and seeds user

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,13 +1,16 @@
-1.upto(1000) do |n|
+companies = %w( Google Amazon Apple GM TOYOTA NEC Panasonic)
+educations = [ "MIT", "Stanford", "Harvard", "Tokyo"]
+
+1.upto(100) do |n|
   User.create!(
-    email: "example_#{n}@gmail.com",
+    email: "example_#{n}@aiit.ac.jp",
     password: "resume_test",
     sign_in_count: 0,
     confirmed_at: DateTime.now,
     user_name: "hal#{n}",
     nick_name: "hal#{n}",
-    company_name: "",
-    education: "",
+    company_name: companies[rand(companies.length)],
+    education: educations[rand(educations.length)],
     gender: rand(0..1),
     age: rand(0..6),
     user_type: rand(0..1)
