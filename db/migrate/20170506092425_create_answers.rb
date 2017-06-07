4f10312d5c420cf5fbeef76c101fc3da76ccb9fd
hal13 <a15z8my@aiit.ac.jp>
2017-06-07 17:22:13 +0000

    add questions answers

--- db/migrate/20170506092425_create_answers.rb
+++ db/migrate/20170506092425_create_answers.rb
@@ -1,11 +1,13 @@
 class CreateAnswers < ActiveRecord::Migration[5.0]
   def change
     create_table :answers do |t|
-      t.integer :answer_type, limit: 2, null: false
+      t.references :question, index: true, null: false
       t.integer :type_id, limit: 2, null: false
       t.string :answer_desc, limit: 64, null: false
 
       t.timestamps
     end
+    
+    add_index :answers, [:question_id, :type_id], unique: true
   end
 end
