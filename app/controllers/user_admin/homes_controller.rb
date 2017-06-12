54c559fba62421d42555e2a5fb0037b534d911ed
prius20 <a1544hm@aiit.ac.jp>
2017-06-13 03:19:48 +0900

    homes_controller.rb

--- app/controllers/user_admin/homes_controller.rb
+++ app/controllers/user_admin/homes_controller.rb
@@ -1,4 +1,10 @@
 class UserAdmin::HomesController < UserAdmin::Base
   def index
+    ###
+    @user_resume = UserResume.find(current_user.id)
+    @user_certificates = UserCertificate.find(current_user.id)
+    @user_competencies = UserCompetency.find(current_user.id)
+    @user_careers = UserCareer.find(current_user.id)
+    @user_skills = UserSkill.find(current_user.id)
   end
 end
